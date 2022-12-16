bcf7fdb1ec15008e774ad00024dc4955af4b290b
rgabrielw <120602524+rgabrielw@users.noreply.github.com>
2022-12-15 23:46:58 -0400

    Add files via upload

---
+++ 20221215_010.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+{
+    float altura = 0;
+    float peso_ideal = 0;
+    
+    printf("Insira a sua altura: ");
+    scanf("%f",& altura);
+    
+    peso_ideal = (72.7*altura)-58;
+    
+    printf("O seu peso ideal é: %.2f kg", peso_ideal);
+    
+    return 0;
+}
