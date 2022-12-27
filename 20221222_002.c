d01c1e32f30cc7b2aa6703e565601a8cf92f475f
rgabrielw <120602524+rgabrielw@users.noreply.github.com>
2022-12-27 15:38:57 -0300

    Add files via upload

---
+++ 20221222_002.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() 
+{
+    int cont, num, maior, menor;
+    
+    for (cont=0; cont<4; cont++){
+        printf("\ndigite o numero: ");
+        scanf("%d",& num);
+        
+		if (cont == 0){
+        	maior = num;
+        	menor = num;
+		}
+        	if (num > maior){
+        		maior = num;
+		} 
+        
+        	if (num < menor){
+        		menor = num;
+		} 
+        
+        
+        
+    }
+    printf("o maior valor eh %d e o menor eh %d", maior, menor);
+    
+return 0;
+}
