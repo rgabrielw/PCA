a84809796f7c7ab3ba2890cf5da5cef6ad82f9ac
rgabrielw <120602524+rgabrielw@users.noreply.github.com>
2022-12-27 18:05:52 -0300

    Add files via upload

---
+++ 20222220007.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main (){
+	float cont, num, maior, menor, media;
+	float soma = 0;
+	
+	for(cont=0; cont<3; cont++){
+		printf("informe a sua nota: ");
+		scanf("%f", &num);
+		soma += num;
+		
+			if (cont == 0){
+				maior = num;
+				menor = num;
+		}
+				if (num > maior){
+					maior = num;
+		}
+				if (num < menor){
+					menor = num;
+		}	
+	}	
+	printf("a maior nota foi %.1f e a menor nota foi %.1f", maior, menor);
+	printf("\na maior e a menor media eh igual a %.2f", soma/3);
+	return 0;
+}
