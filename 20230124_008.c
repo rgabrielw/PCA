cbdcec8bb5414437e2adaf1e0f76c1703ae58a95
rgabrielw <120602524+rgabrielw@users.noreply.github.com>
2023-01-25 20:09:57 -0400

    Add files via upload

---
+++ 20230124_008.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int i, j, tam, pe;
+
+void produto_escalar(){
+	
+	int numx[tam];
+	int numy[tam];
+		
+	for(i=1; i<=tam; i++){
+		printf("digite os valores de x: ");
+		scanf("%d", &numx[i]);
+	}
+	for(i=1; i<=tam; i++){
+		printf("digite os valores de y: ");
+		scanf("%d", &numy[i]);
+	}
+	for(i=1; i<=tam; i++){
+		pe += numx[i] * numy[i];
+	} 
+	printf("\no produto escalar dos dois vetores eh %d", pe);
+	return;
+}
+
+int main(int argc, char *argv[]){
+	
+	printf("digite o tamanho dos vetores: ");
+	scanf("%d", &tam);
+	
+	while(tam<1){
+		printf("numero invalido! digite um numero positivo: ");
+		scanf("%d", &tam);
+	}		
+	produto_escalar();
+	
+	return 0;
+}
