e73b0cad7b284f7330ee31540db2685d11d19471
rgabrielw <120602524+rgabrielw@users.noreply.github.com>
2023-02-09 11:57:43 -0400

    Update 20230126_008.c

--- 20230126_008.c
+++ 20230126_008.c
@@ -3,11 +3,12 @@
 void funcao(int mat1[3][3], int mat2[3][3], int mat3[3][3]){
 	int M1L=3, M1C=3, M2L=3, M2C=3;
   	int linha, coluna, i, somaproduto;
-	for(linha=0; linha<M1L; linha++)
-    	for(coluna=0; coluna<M2C; coluna++){
-      		somaproduto=0;
-      		for(i=0; i<M1L; i++) somaproduto+=mat1[linha][i]*mat2[i][coluna];
-      			mat3[linha][coluna] = somaproduto;
+	for(linha=0; linha<M1L; linha++){
+    		for(coluna=0; coluna<M2C; coluna++){
+      			somaproduto=0;
+      			for(i=0; i<M1L; i++) somaproduto+=mat1[linha][i]*mat2[i][coluna];
+      				mat3[linha][coluna] = somaproduto;
+		}
     	}
 }
 int main(int argc, char *argv[]){ 
